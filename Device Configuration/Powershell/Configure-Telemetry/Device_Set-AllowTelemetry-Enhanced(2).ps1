feb837af830e1ac6e79403cfaeffdf452e27e83d
Olav Rønnestad Birkeland <olavbirkeland@hotmail.com>
2018-02-21 11:18:54 +0100

    Set-AllowTelemetry
    Added two scripts to set telemetry level.

---
+++ Device Configuration/Powershell/Configure-Telemetry/Device_Set-AllowTelemetry-Enhanced(2).ps1
@@ -0,0 +1,103 @@
+﻿<#
+
+.SYNOPSIS
+    Sets AllowTelemetry (Telemetry Level) to Enhanced (2)
+
+.DESCRIPTION
+    Sets AllowTelemetry (Telemetry Level) to Enhanced (2)
+
+.NOTES
+You need to run this script in the DEVICE context in Intune.
+
+#>
+
+
+#Change the app name
+$AppName = 'Device_Set-TelemetryLevel-Enhanced(2)'
+$Timestamp = Get-Date -Format 'HHmmssffff'
+$LogDirectory = ('{0}\Program Files\IronstoneIT\Intune\DeviceConfiguration' -f $env:SystemDrive)
+$Transcriptname = ('{2}\{0}_{1}.txt' -f $AppName, $Timestamp, $LogDirectory)
+$ErrorActionPreference = 'continue'
+
+if (!(Test-Path -Path $LogDirectory)) {
+    New-Item -ItemType Directory -Path $LogDirectory -ErrorAction Stop
+}
+
+Start-Transcript -Path $Transcriptname
+#Wrap in a try/catch, so we can always end the transcript
+Try {
+    # Get the ID and security principal of the current user account
+    $myWindowsID = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $myWindowsPrincipal = new-object -TypeName System.Security.Principal.WindowsPrincipal -ArgumentList ($myWindowsID)
+ 
+    # Get the security principal for the Administrator role
+    $adminRole = [Security.Principal.WindowsBuiltInRole]::Administrator
+ 
+    # Check to see if we are currently running "as Administrator"
+    if (!($myWindowsPrincipal.IsInRole($adminRole))) {
+        # We are not running "as Administrator" - so relaunch as administrator
+   
+        # Create a new process object that starts PowerShell
+        $newProcess = new-object -TypeName System.Diagnostics.ProcessStartInfo -ArgumentList 'PowerShell'
+   
+        # Specify the current script path and name as a parameter
+        $newProcess.Arguments = $myInvocation.MyCommand.Definition
+   
+        # Indicate that the process should be elevated
+        $newProcess.Verb = 'runas'
+   
+        # Start the new process
+        [Diagnostics.Process]::Start($newProcess)
+   
+        # Exit from the current, unelevated, process
+        Write-Output -InputObject 'Restart in elevated'
+        exit
+   
+    }
+
+    #64-bit invocation
+    if ($env:PROCESSOR_ARCHITEW6432 -eq 'AMD64') {
+        write-Output -InputObject "Y'arg Matey, we're off to the 64-bit land....."
+        if ($myInvocation.Line) {
+            &"$env:WINDIR\sysnative\windowspowershell\v1.0\powershell.exe" -NonInteractive -NoProfile $myInvocation.Line
+        }
+        else {
+            &"$env:WINDIR\sysnative\windowspowershell\v1.0\powershell.exe" -NonInteractive -NoProfile -file ('{0}' -f $myInvocation.InvocationName) $args
+        }
+        exit $lastexitcode
+    }
+ 
+ 
+ 
+    ##########################################
+    [string] $Local:PathRegDir     = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection'
+    [string[]] $Local:NameRegKeys  = @('AllowTelemetry','AllowTelemetry_PolicyManager')
+    [byte]   $Local:TelemetryLevel = 2
+    foreach ($Key in $Local:NameRegKeys) {
+        Set-ItemProperty -Path $Local:PathRegDir -Name $Key -Value $Local:TelemetryLevel -Type 'DWord' -Force
+    }
+    ##########################################
+ 
+ 
+    
+}
+Catch {
+    # Construct Message
+    $ErrorMessage = 'Unable to set TelemetryLevel.'
+    $ErrorMessage += " `n"
+    $ErrorMessage += 'Exception: '
+    $ErrorMessage += $_.Exception
+    $ErrorMessage += " `n"
+    $ErrorMessage += 'Activity: '
+    $ErrorMessage += $_.CategoryInfo.Activity
+    $ErrorMessage += " `n"
+    $ErrorMessage += 'Error Category: '
+    $ErrorMessage += $_.CategoryInfo.Category
+    $ErrorMessage += " `n"
+    $ErrorMessage += 'Error Reason: '
+    $ErrorMessage += $_.CategoryInfo.Reason
+    Write-Error -Message $ErrorMessage
+}
+Finally {
+    Stop-Transcript
+}
