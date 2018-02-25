df7947b3d7a21549a2b503d96c3fc70b686e0e54
Morten Reinhardtsen <morten.reinhardtsen@outlook.com>
2018-02-25 21:30:24 +0100

    Removed unused code

--- PowershellWrapper/Applications/Get-IntuneApplication.ps1
+++ PowershellWrapper/Applications/Get-IntuneApplication.ps1
@@ -1,6 +1,6 @@
-Function Get-IntuneApplication(){
+Function Get-IntuneApplication() {
 
-<#
+    <#
 .SYNOPSIS
 This function is used to get applications from the Graph API REST interface
 .DESCRIPTION
@@ -12,29 +12,29 @@ Returns any applications configured in Intune
 NAME: Get-IntuneApplication
 #>
 
-[cmdletbinding()]
+    [cmdletbinding()]
 
-param
-(
-    $Name
-)
+    param
+    (
+        $Name
+    )
 
-$graphApiVersion = "v1.0"
-$Resource = "deviceAppManagement/mobileApps"
+    $graphApiVersion = 'v1.0'
+    $Resource = 'deviceAppManagement/mobileApps'
 
     try {
 
-        if($Name){
+        if ($Name) {
 
-        $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)"
-        (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).Value | Where-Object { ($_.'displayName').contains("$Name") -and (!($_.'@odata.type').Contains("managed")) -and (!($_.'@odata.type').Contains("#microsoft.graph.iosVppApp")) }
+            $uri = ('https://graph.microsoft.com/{0}/{1}' -f $graphApiVersion, ($Resource))
+            (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).Value | Where-Object { ($_.'displayName').contains(('{0}' -f $Name)) -and (!($_.'@odata.type').Contains('managed')) -and (!($_.'@odata.type').Contains('#microsoft.graph.iosVppApp')) }
 
         }
 
         else {
 
-        $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)"
-        (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).Value | Where-Object { (!($_.'@odata.type').Contains("managed")) -and (!($_.'@odata.type').Contains("#microsoft.graph.iosVppApp")) }
+            $uri = ('https://graph.microsoft.com/{0}/{1}' -f $graphApiVersion, ($Resource))
+            (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).Value | Where-Object { (!($_.'@odata.type').Contains('managed')) -and (!($_.'@odata.type').Contains('#microsoft.graph.iosVppApp')) }
 
         }
 
@@ -42,17 +42,17 @@ $Resource = "deviceAppManagement/mobileApps"
 
     catch {
 
-    $ex = $_.Exception
-    Write-Host "Request to $Uri failed with HTTP Status $([int]$ex.Response.StatusCode) $($ex.Response.StatusDescription)" -f Red
-    $errorResponse = $ex.Response.GetResponseStream()
-    $reader = New-Object System.IO.StreamReader($errorResponse)
-    $reader.BaseStream.Position = 0
-    $reader.DiscardBufferedData()
-    $responseBody = $reader.ReadToEnd();
-    Write-Host "Response content:`n$responseBody" -f Red
-    Write-Error "Request to $Uri failed with HTTP Status $($ex.Response.StatusCode) $($ex.Response.StatusDescription)"
-    write-host
-    break
+        $ex = $_.Exception
+        Write-Verbose -Message ('Request to {0} failed with HTTP Status {1} {2}' -f $Uri, ([int]$ex.Response.StatusCode), $ex.Response.StatusDescription)
+        $errorResponse = $ex.Response.GetResponseStream()
+        $reader = New-Object -TypeName System.IO.StreamReader -ArgumentList ($errorResponse)
+        $reader.BaseStream.Position = 0
+        $reader.DiscardBufferedData()
+        $responseBody = $reader.ReadToEnd()
+
+        Write-Verbose -Message ("Response content:`n{0}" -f $responseBody)
+        Write-Error -Message ('Request to {0} failed with HTTP Status {1} {2}' -f $Uri, $ex.Response.StatusCode, $ex.Response.StatusDescription)
+        break
 
     }
 
