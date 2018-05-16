4770a6ba0e2206d38675cabc43860c4838b49e3a
Olav Rønnestad Birkeland <olavbirkeland@hotmail.com>
2018-05-16 16:55:27 +0200

    Added a generic Intune MDM PowerShell Template

---
+++ Templates/Generic Intune MDM PowerShell Template.ps1
@@ -0,0 +1,117 @@
+﻿<#
+
+.SYNOPSIS
+
+
+.DESCRIPTION
+
+
+.NOTES
+    * In Intune, remember to set "Run this script using the logged in credentials"  according to the $DeviceContext variable.
+        * Intune -> Device Configuration -> PowerShell Scripts -> $NameScriptFull -> Properties -> "Run this script using the logged in credentials"
+        * DEVICE (Local System) or USER (Logged in user).
+    * Only edit $NameScript and add your code in the #region Your Code Here.
+    * You might want to touch "Settings - PowerShell - Output Preferences" for testing / development. 
+        * $VerbosePreference, eventually $DebugPreference, to 'Continue' will print much more details.
+
+#>
+
+
+# Script Variables
+[bool]   $DeviceContext = $true
+[string] $NameScript    = 'Verb-Noun'
+
+# Settings - PowerShell - Output Preferences
+$DebugPreference       = 'SilentlyContinue'
+$InformationPreference = 'SilentlyContinue'
+$VerbosePreference     = 'SilentlyContinue'
+$WarningPreference     = 'Continue'
+
+
+
+#region    Don't Touch This
+# Settings - PowerShell - Interaction
+$ConfirmPreference     = 'None'
+$ProgressPreference    = 'SilentlyContinue'
+
+# Settings - PowerShell - Behaviour
+$ErrorActionPreference = 'Continue'
+
+# Dynamic Process Variables
+[string] $NameScriptFull      = ('{0}_{1}' -f ($(if($DeviceContext){'Device'}Else{'User'}),$NameScript))
+[string] $ProcessArchitecture = $(if([System.Environment]::Is64BitProcess){'64'}Else{'32'})
+[string] $OSArchitecture      = $(if([System.Environment]::Is64BitOperatingSystem){'64'}Else{'32'})
+
+# Dynamic Logging Variables
+$Timestamp    = [DateTime]::Now.ToString('yyMMdd-HHmmssffff')
+$PathDirLog   = ('{0}\IronstoneIT\Intune\DeviceConfiguration\' -f ($(if($DeviceContext){$env:ProgramW6432}else{$env:APPDATA})))
+$PathFileLog  = ('{0}{1}-{2}bit-{3}.txt' -f ($PathDirLog,$NameScriptFull,$ProcessArchitecture,$Timestamp))
+
+# Start Transcript
+if (-not(Test-Path -Path $PathDirLog)) {New-Item -ItemType 'Directory' -Path $PathDirLog -ErrorAction 'Stop'}
+Start-Transcript -Path $PathFileLog
+
+
+# Wrap in Try/Catch, so we can always end the transcript
+Try {
+    # Output Process and OS Architecture Info
+    Write-Output -InputObject ('PowerShell is running as a {0} bit process on a {1} bit OS.' -f ($ProcessArchitecture,$OSArchitecture))
+    
+
+    # If OS is 64 bit, and PowerShell got launched as x86, relaunch as x64
+    if ( (-not([System.Environment]::Is64BitProcess))  -and [System.Environment]::Is64BitOperatingSystem) {
+        write-Output -InputObject (' * Will restart this PowerShell session as x64.')
+        if ($myInvocation.Line) {
+            &"$env:WINDIR\sysnative\windowspowershell\v1.0\powershell.exe" -NonInteractive -NoProfile $myInvocation.Line
+        }
+        else {
+            &"$env:WINDIR\sysnative\windowspowershell\v1.0\powershell.exe" -NonInteractive -NoProfile -File ('{0}' -f $myInvocation.InvocationName) $args
+        }
+        exit $lastexitcode
+    }
+#endregion Don't Touch This
+
+
+
+
+################################################
+#region    Your Code Here
+################################################  
+    
+
+
+
+    
+
+    
+    
+################################################
+#endregion Your Code Here
+################################################   
+
+
+
+
+#region    Don't touch this
+}
+Catch {
+    # Construct Message
+    $ErrorMessage = ('{0} finished with errors:' -f ($NameScriptFull))
+    $ErrorMessage += " `n"
+    $ErrorMessage += 'Exception: '
+    $ErrorMessage += $_.Exception
+    $ErrorMessage += " `n"
+    $ErrorMessage += 'Activity: '
+    $ErrorMessage += $_.CategoryInfo.Activity
+    $ErrorMessage += " `n"
+    $ErrorMessage += 'Error Category: '
+    $ErrorMessage += $_.CategoryInfo.Category
+    $ErrorMessage += " `n"
+    $ErrorMessage += 'Error Reason: '
+    $ErrorMessage += $_.CategoryInfo.Reason
+    Write-Error -Message $ErrorMessage
+}
+Finally {
+    Stop-Transcript
+}
+#endregion Don't touch this
