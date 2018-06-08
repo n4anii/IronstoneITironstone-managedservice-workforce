3e90a2f3fccf9b4aad87fd69e8f58d068d7ab56c
Olav Rønnestad Birkeland <olavbirkeland@hotmail.com>
2018-06-08 16:39:55 +0200

    Updated Template

--- Templates/Generic Intune MDM PowerShell Template.ps1
+++ Templates/Generic Intune MDM PowerShell Template.ps1
@@ -28,7 +28,6 @@ $VerbosePreference     = 'SilentlyContinue'
 $WarningPreference     = 'Continue'
 
 
-
 #region    Don't Touch This
 # Settings - PowerShell - Interaction
 $ConfirmPreference     = 'None'
@@ -37,27 +36,43 @@ $ProgressPreference    = 'SilentlyContinue'
 # Settings - PowerShell - Behaviour
 $ErrorActionPreference = 'Continue'
 
-# Dynamic Process Variables
-[string] $NameScriptFull      = ('{0}_{1}' -f ($(if($DeviceContext){'Device'}Else{'User'}),$NameScript))
-[string] $ProcessArchitecture = $(if([System.Environment]::Is64BitProcess){'64'}Else{'32'})
-[string] $OSArchitecture      = $(if([System.Environment]::Is64BitOperatingSystem){'64'}Else{'32'})
+# Dynamic Variables - Process & Environment
+[string] $NameScriptFull      = ('{0}_{1}' -f ($(if($DeviceContext){'Device'}else{'User'}),$NameScript))
+[string] $NameScriptVerb      = $NameScript.Split('-')[0]
+[string] $NameScriptNoun      = $NameScript.Split('-')[-1]
+[string] $ProcessArchitecture = $(if([System.Environment]::Is64BitProcess){'64'}else{'32'})
+[string] $OSArchitecture      = $(if([System.Environment]::Is64BitOperatingSystem){'64'}else{'32'})
+
+# Dynamic Variables - User
+[string] $StrIsAdmin       = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+[string] $StrUserName      = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+[string] $SidCurrentUser   = [System.Security.Principal.WindowsIdentity]::GetCurrent().User.Value
+[string] $SidSystemUser    = 'S-1-5-18'
+[bool] $CurrentUserCorrect = $(
+    if($DeviceContext -and $SIDCurrentUser -eq $SIDSystemUser){$true}
+    elseif (-not($DeviceContext) -and $SIDCurrentUser -ne $SIDSystemUser){$true}
+    else {$false}
+)
 
 # Dynamic Logging Variables
 $Timestamp    = [DateTime]::Now.ToString('yyMMdd-HHmmssffff')
-$PathDirLog   = ('{0}\IronstoneIT\Intune\DeviceConfiguration\' -f ($(if($DeviceContext){$env:ProgramW6432}else{$env:APPDATA})))
+$PathDirLog   = ('{0}\IronstoneIT\Intune\DeviceConfiguration\' -f ($(if($DeviceContext -and $CurrentUserCorrect){$env:ProgramW6432}else{$env:APPDATA})))
 $PathFileLog  = ('{0}{1}-{2}bit-{3}.txt' -f ($PathDirLog,$NameScriptFull,$ProcessArchitecture,$Timestamp))
 
 # Start Transcript
 if (-not(Test-Path -Path $PathDirLog)) {New-Item -ItemType 'Directory' -Path $PathDirLog -ErrorAction 'Stop'}
 Start-Transcript -Path $PathFileLog
 
+# Output User Info, Exit if not $CurrentUserCorrect
+Write-Output -InputObject ('Running as user "{0}". Has admin privileges? {1}. $DeviceContext = {2}. Running as correct user? {3}.' -f ($StrUserName,$StrIsAdmin,$DeviceContext.ToString(),$CurrentUserCorrect.ToString()))
+if (-not($CurrentUserCorrect)){Throw 'Not running as correct user!'} 
+
+# Output Process and OS Architecture Info
+Write-Output -InputObject ('PowerShell is running as a {0} bit process on a {1} bit OS.' -f ($ProcessArchitecture,$OSArchitecture))
+
 
 # Wrap in Try/Catch, so we can always end the transcript
-Try {
-    # Output Process and OS Architecture Info
-    Write-Output -InputObject ('PowerShell is running as a {0} bit process on a {1} bit OS.' -f ($ProcessArchitecture,$OSArchitecture))
-    
-
+Try {    
     # If OS is 64 bit, and PowerShell got launched as x86, relaunch as x64
     if ( (-not([System.Environment]::Is64BitProcess))  -and [System.Environment]::Is64BitOperatingSystem) {
         write-Output -InputObject (' * Will restart this PowerShell session as x64.')
@@ -65,50 +80,46 @@ Try {
             &"$env:WINDIR\sysnative\windowspowershell\v1.0\powershell.exe" -NonInteractive -NoProfile $myInvocation.Line
         }
         else {
-            &"$env:WINDIR\sysnative\windowspowershell\v1.0\powershell.exe" -NonInteractive -NoProfile -File ('{0}' -f $myInvocation.InvocationName) $args
+            &"$env:WINDIR\sysnative\windowspowershell\v1.0\powershell.exe" -NonInteractive -NoProfile -File ('{0}' -f ($myInvocation.InvocationName)) $args
         }
         exit $lastexitcode
     }
+    
+    # End the Initialize Region
+    Write-Output -InputObject ('**********************')
 #endregion Don't Touch This
-
-
-
-
 ################################################
 #region    Your Code Here
-################################################  
-    
+################################################
+
+
+
 
 
 
     
 
-    
-    
 ################################################
 #endregion Your Code Here
 ################################################   
-
-
-
-
 #region    Don't touch this
 }
 Catch {
     # Construct Message
-    $ErrorMessage = ('{0} finished with errors:' -f ($NameScriptFull))
-    $ErrorMessage += " `n"
+    [string] $ErrorMessage = ('{0} finished with errors:' -f ($NameScriptFull))
+    $ErrorMessage += "`r`n"
     $ErrorMessage += 'Exception: '
     $ErrorMessage += $_.Exception
-    $ErrorMessage += " `n"
+    $ErrorMessage += "`r`n"
     $ErrorMessage += 'Activity: '
     $ErrorMessage += $_.CategoryInfo.Activity
-    $ErrorMessage += " `n"
+    $ErrorMessage += "`r`n"
     $ErrorMessage += 'Error Category: '
     $ErrorMessage += $_.CategoryInfo.Category
-    $ErrorMessage += " `n"
+    $ErrorMessage += "`r`n"
     $ErrorMessage += 'Error Reason: '
     $ErrorMessage += $_.CategoryInfo.Reason
+    # Write Error Message
     Write-Error -Message $ErrorMessage
 }
 Finally {
