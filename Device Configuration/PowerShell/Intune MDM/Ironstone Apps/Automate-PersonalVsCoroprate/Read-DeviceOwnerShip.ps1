ad788163ae9724fe3df1dfb42be61e8e2ed428a3
Olav Rønnestad Birkeland <o-l-a-v@users.noreply.github.com>
2019-03-14 19:39:25 +0100

    Create Read-DeviceOwnerShip.ps1

---
+++ Device Configuration/PowerShell/Intune MDM/Ironstone Apps/Automate-PersonalVsCoroprate/Read-DeviceOwnerShip.ps1
@@ -0,0 +1,9 @@
+﻿# Assets
+$Path  = [string]$('Registry::HKEY_LOCAL_MACHINE\SOFTWARE\IronstoneIT\Intune\DeviceInfo')
+$Name  = [string]$('DeviceOwnership')
+
+# Read
+[string]$($Val=Get-ItemProperty -Path $Path -Name $Name -ErrorAction 'SilentlyContinue';if($?){$Val}else{''})
+
+# Oneliner
+[string]$($Val=Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\IronstoneIT\Intune\DeviceInfo' -Name 'DeviceOwnership' -ErrorAction 'SilentlyContinue';if($?){$Val}else{''})
