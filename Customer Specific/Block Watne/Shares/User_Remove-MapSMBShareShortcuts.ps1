af4e70a02861c8b5bebc24afbb3f6e3670418287
Olav Rønnestad Birkeland <o-l-a-v@users.noreply.github.com>
2020-06-25 09:46:35 +0200

    Update User_Remove-MapSMBShareShortcuts.ps1

--- Customer Specific/Block Watne/Shares/User_Remove-MapSMBShareShortcuts.ps1
+++ Customer Specific/Block Watne/Shares/User_Remove-MapSMBShareShortcuts.ps1
@@ -1,24 +1,46 @@
+<#
+    .SYNOPSIS
+        Removes shortcuts and script files for the SMB mapping script.
+#>
+
+
+
+
 # Help variable
-$Success = [bool]$($true)
+$Success = [bool] $true
+$ErrorActionPreference = 'Stop'
 
 
-# v1
-## Asset
-$Path = [string]$('{0}\Tools' -f ([Environment]::GetFolderPath('Desktop')))
-## Remove if exist
-if ([System.IO.Directory]::Exists($Path)) {
-    [System.IO.Directory]::Delete($Path,$true)
-    if (-not $?) {
-        $Success = [bool] $false
+
+
+################
+Try {
+################
+# Directories
+## Assets
+$Paths = [string[]](
+    ('{0}\IronstoneIT\Intune\Scripts\SMB' -f ($env:LOCALAPPDATA)),                # Script files
+    ('{0}\Tools' -f ([Environment]::GetFolderPath('Desktop'))),                   # v1 of the SMB shortcut script
+    ('{0}\Koble til fellesdisker' -f ([Environment]::GetFolderPath('Desktop')))   # v3 of the SMB shortcut script
+)
+## Remove
+foreach ($Path in $Paths) {
+    if ([System.IO.Directory]::Exists($Path)) {
+        [System.IO.Directory]::Delete($Path,$true)
+        if (-not $?) {
+            $Success = [bool] $false
+        }
     }
 }
 
 
-# v2
-## Asset
-$Path = [string]$('{0}' -f ([Environment]::GetFolderPath('Desktop')))
-$Name = [string]$('Koble til ``*´´.lnk')
-## Remove if exist
+
+
+## v3 of the SMB shortcut script
+### Asset
+$Path = [string]('{0}' -f ([System.Environment]::GetFolderPath('Desktop')))
+$Name = [string]('Koble til ``*´´.lnk')
+### Remove if exist
 [string[]]$(Get-ChildItem -Path $Path -Filter $Name -Depth 0 -Recurse:$false -File -ErrorAction 'SilentlyContinue' | Select-Object -ExpandProperty 'FullName').ForEach{
     $null = [System.IO.File]::Delete($_)
     if (-not $?) {
@@ -26,6 +48,17 @@ $Name = [string]$('Koble til ``*´´.lnk')
         break
     }
 }
+################
+}
+################
+
+
+
+
+Catch {
+    $Success = [bool] $false
+}
+
 
 
 # Exit
