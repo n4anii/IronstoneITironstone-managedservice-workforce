46884c6422c4e299d0d5798746b295444a1031ce
Peter Korsmo <peter.korsmo@outlook.com>
2023-03-17 12:12:50 +0100

    spelling error fix

--- CWF2/ProactiveRemediation/Diskspace-Detection.ps1
+++ CWF2/ProactiveRemediation/Diskspace-Detection.ps1
@@ -1,10 +1,10 @@
 $disk = Get-CimInstance -ClassName Win32_LogicalDisk | Where-Object DeviceID -EQ "C:"
 
 if($disk.FreeSpace -le 20000000000){
-    Write-Output -InputObject "Discspace below threshold, remediate problem"
+    Write-Output -InputObject "Diskspace below threshold, remediate problem"
     Exit 1
 }
 else{
-    Write-Output -InputObject "Discspace ok"
+    Write-Output -InputObject "Diskspace ok"
     Exit 0
 }
