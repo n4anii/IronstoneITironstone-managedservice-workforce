3f9ab72c6b9b3bc28c44349f1aa764e7c492cd99
Peter Korsmo <peter.korsmo@outlook.com>
2023-03-29 09:43:31 +0200

    update test to include documents

--- Customer Specific/Aksio/SPAKTTEST.ps1
+++ Customer Specific/Aksio/SPAKTTEST.ps1
@@ -37,7 +37,8 @@ function Sync-SharepointLocation {
     }
     else {
         Write-Host "Sync completed."
-        while (!(Get-ChildItem -Path $syncPath -ErrorAction SilentlyContinue)) {
+        while (!(Get-ChildItem -Path $syncPath -ErrorAction SilentlyContinue) -and !(Get-ChildItem -Path $syncPath.Replace("Dokumenter", "Documents") -ErrorAction SilentlyContinue)) {
+            Write-Host "Waiting for folder creation"
             Start-Sleep -Seconds 2
         }
         return $true
@@ -93,9 +94,11 @@ if ($false -eq (Test-Path -Path "C:\jottacloud")) {
 
 if($false -eq (Test-Path -Path "C:\jottacloud\analysetjenester")){
     if (Test-Path -Path "$home\Aksio InsurTech AS\Prosjekter - Dokumenter") {
+        Write-Host "Dokumenter funnet"
         New-Item -ItemType SymbolicLink -Path "C:\jottacloud\analysetjenester" -Value "$home\Aksio InsurTech AS\Prosjekter - Dokumenter" -Force
     }
     elseif (Test-Path -Path "$home\Aksio InsurTech AS\Prosjekter - Documents") {
+        Write-Host "Documents found"
         New-Item -ItemType SymbolicLink -Path "C:\jottacloud\analysetjenester" -Value "$home\Aksio InsurTech AS\Prosjekter - Documents" -Force
     }
 }
