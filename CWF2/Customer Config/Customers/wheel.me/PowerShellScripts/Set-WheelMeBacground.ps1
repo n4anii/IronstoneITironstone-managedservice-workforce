5df9003790b0950cadefe5551fc2d5bc98ab926d
emileggen <etweggen@gmail.com>
2023-04-25 12:44:30 +0200

    Wheel.me before cleanup

---
+++ CWF2/Customer Config/Customers/wheel.me/PowerShellScripts/Set-WheelMeBacground.ps1
@@ -0,0 +1,23 @@
+﻿$RegKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP"
+
+$DesktopPath = "DesktopImagePath"
+$DesktopStatus = "DesktopImageStatus"
+$DesktopUrl = "DesktopImageUrl"
+$DesktopImageValue = "C:\Windows\Web\Wallpaper\Windows\Wheelme-Wallpaper.png"
+
+$url = "https://workforcebranding.blob.core.windows.net/background/Wheel.me-Wallpaper.png"
+
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $DesktopImageValue)
+
+if (!(Test-Path $RegKeyPath))
+{
+Write-Host "Creating registry path $($RegKeyPath)."
+New-Item -Path $RegKeyPath -Force | Out-Null
+}
+
+New-ItemProperty -Path $RegKeyPath -Name $DesktopStatus -Value 1 -PropertyType DWORD -Force | Out-Null
+New-ItemProperty -Path $RegKeyPath -Name $DesktopPath -Value $DesktopImageValue -PropertyType STRING -Force | Out-Null
+New-ItemProperty -Path $RegKeyPath -Name $DesktopUrl -Value $DesktopImageValue -PropertyType STRING -Force | Out-Null
+
+RUNDLL32.EXE USER32.DLL, UpdatePerUserSystemParameters 1, True
