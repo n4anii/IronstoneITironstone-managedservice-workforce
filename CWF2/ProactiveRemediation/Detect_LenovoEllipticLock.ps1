6219f92a04878b29c6390dd79fd0463b208afec2
emileggen <etweggen@gmail.com>
2023-07-19 10:42:40 +0200

    Add - Remediation Lenovo Lock

---
+++ CWF2/ProactiveRemediation/Detect_LenovoEllipticLock.ps1
@@ -0,0 +1,11 @@
+﻿try {
+    $service = Get-Service -Name "LenovoEllipticVirtualLock" -ErrorAction Stop
+    if ($null -ne $service) {
+        Write-Host "Service exists"
+        exit 1
+    }
+}
+catch {
+    Write-Host "Service does not exist"
+    exit 0
+}
---
+++ CWF2/ProactiveRemediation/Remediation_LenovoEllipticLock.ps1
@@ -0,0 +1,18 @@
+﻿try {
+    $service = Get-Service -Name "LenovoEllipticVirtualLock" -ErrorAction Stop
+    $serviceStartType = (Get-WmiObject -Query "Select * From Win32_Service Where Name='LenovoEllipticVirtualLock'").StartMode
+
+    if ($service.Status -eq 'Running') {
+        Stop-Service -Name "LenovoEllipticVirtualLock" -Force
+        Write-Host "Service stopped"
+    }
+
+    if ($serviceStartType -ne 'Disabled') {
+        Set-Service -Name "LenovoEllipticVirtualLock" -StartupType Disabled
+        Write-Host "Service startup type set to Disabled"
+    }
+}
+catch {
+    Write-Host "Service does not exist or failed to modify. Error: $_"
+    exit 1
+}
