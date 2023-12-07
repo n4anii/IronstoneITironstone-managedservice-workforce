3a4d9777e9efa0650a10ab2c8ebcf606a804f504
peterjk <peter.korsmo@gmail.com>
2023-12-07 14:16:55 +0100

    Convert user creation to Mgraph

--- Migration kit/Scripts/CreateUsersInDestination.ps1
+++ Migration kit/Scripts/CreateUsersInDestination.ps1
@@ -1,14 +1,46 @@
-#Connect-AzureAD
+# Specify the required permissions
+$scopes = "Directory.ReadWrite.All"
+# "User.ReadWrite.All", "Group.ReadWrite.All", 
+# Connect to Microsoft Graph with the specified permissions
+Connect-MgGraph -Scopes $scopes
 
-$users = Import-Csv "..\import\masterdata.csv" -Delimiter ';'
 
-$passwordprofile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
-$passwordprofile.ForceChangePasswordNextLogin = $true
+# Import users from CSV file
+$users = Import-Csv "..\import\masterdata.csv" -Delimiter ','
 
 foreach ($user in $users) {
-    $passwordprofile.Password = $user.Password
-    New-AzureADUser -UserPrincipalName $user.destinationTmpUPN -PasswordProfile $passwordprofile -GivenName $user.firstName -Surname $user.lastName -DisplayName $user.destinationDisplayName -UsageLocation "NO" -AccountEnabled $true -MailNickName $user.destinationUPN.Split('@')[0]
+    Write-Output $user.sourceDisplayName
+    Write-Output $user.destinationTmpUPN
+    Write-Output $user.destinationUPN.Split('@')[0]
+    if ($null -eq (Get-MgUser -UserId $user.destinationTmpUPN -ErrorAction SilentlyContinue)) {
+        # Create a new user object
+        Write-Output "Create User"
+        $newUser = @{
+            AccountEnabled    = $true
+            DisplayName       = $user.destinationDisplayName
+            MailNickname      = $user.destinationUPN.Split('@')[0]
+            UserPrincipalName = $user.destinationTmpUPN
+            PasswordProfile   = @{
+                ForceChangePasswordNextSignIn = $true
+                Password                      = $user.Password
+            }
+            GivenName         = $user.firstName
+            Surname           = $user.lastName
+            UsageLocation     = $user.UsageLocation
+        }
 
-    $group = Get-AzureADGroup -SearchString $user.licenseGroup
-    Add-AzureADGroupMember -ObjectId $group.ObjectId -RefObjectId (Get-AzureADUser -SearchString $user.destinationTmpUPN).ObjectId
+        # Create a new user in Azure AD
+        $createdUser = New-MgUser -BodyParameter $newUser
+
+        # Get the group by searching for its display name
+        $group = Get-MgGroup -Filter "displayName eq '$($user.licenseGroup)'"
+
+        # Add the user to the group
+        if ($group) {
+            New-MgGroupMember -GroupId $group.Id -DirectoryObjectId $createdUser.Id
+        }
+    }
+    else {
+        Write-Output "Users exits"
+    }
 }
