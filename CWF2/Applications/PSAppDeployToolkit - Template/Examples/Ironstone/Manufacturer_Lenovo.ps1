4a7ae561950a53b0c9728c415507720ec867659a
Herman Bergsløkken <herman.bergslokken@ironstoneit.com>
2024-08-20 13:33:44 +0200

    Get-CimInstance instead of depricated Get-WmiObject

--- CWF2/Applications/PSAppDeployToolkit - Template/Examples/Ironstone/Manufacturer_Lenovo.ps1
+++ CWF2/Applications/PSAppDeployToolkit - Template/Examples/Ironstone/Manufacturer_Lenovo.ps1
@@ -4,13 +4,13 @@
     SystemSKU = $null
 }
 
-$ComputerManufacturer = (Get-WmiObject -Class "Win32_ComputerSystem" | Select-Object -ExpandProperty Manufacturer).Trim()
+$ComputerManufacturer = (Get-CimInstance -ClassName "Win32_ComputerSystem" | Select-Object -ExpandProperty Manufacturer).Trim()
 switch -Wildcard ($ComputerManufacturer) {
     "*Lenovo*" {
         $ComputerDetails.Manufacturer = "Lenovo"
-	    $ComputerDetails.Model = (Get-WmiObject -Class "Win32_ComputerSystemProduct" | Select-Object -ExpandProperty Version).Trim()
-	    $ComputerDetails.SystemSKU = ((Get-WmiObject -Class "Win32_ComputerSystem" | Select-Object -ExpandProperty Model).SubString(0, 4)).Trim()
-	}
+        $ComputerDetails.Model = (Get-CimInstance -ClassName "Win32_ComputerSystemProduct" | Select-Object -ExpandProperty Version).Trim()
+        $ComputerDetails.SystemSKU = ((Get-CimInstance -ClassName "Win32_ComputerSystem" | Select-Object -ExpandProperty Model).SubString(0, 4)).Trim()
+    }
 }
 
 $isValidModel = $ComputerDetails.Manufacturer -eq "Lenovo" -and (
