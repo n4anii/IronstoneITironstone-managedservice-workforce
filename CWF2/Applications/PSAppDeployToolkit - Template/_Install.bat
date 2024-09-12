a7bb77c33331d28c2dd13a723adbf6571109d4d9
Herman Bergsløkken <herman.bergslokken@ironstoneit.com>
2024-09-12 15:10:32 +0200

    Gave up solving spaces with psexec

--- CWF2/Applications/PSAppDeployToolkit - Template/_Install.bat
+++ CWF2/Applications/PSAppDeployToolkit - Template/_Install.bat
@@ -105,15 +105,15 @@ TIMEOUT 10 >NUL
 GOTO :MENU
 
 :Install_silent_winget_psexec
-SET /P WingetFriendlyName=Enter Winget Friendly Name: 
+SET /P WingetFriendlyName=Enter Winget Friendly Name(Cannot contain spaces!): 
 SET /P WingetID=Enter Winget ID: 
 ECHO install_silent_winget_psexec
 IF "%_SANDBOX%"=="Yes" (
     ECHO Machine is detected as Sandbox
     ECHO Friendly warning: Winget is not supported in Sandbox. So this will fail!
-    "%~dp0PsExec64.exe" \\localhost /accepteula /s "\"%~dp0Toolkit\Deploy-Application.exe\" -DeploymentType Install -DeployMode Silent -AllowRebootPassThru -WingetFriendlyName \"%WingetFriendlyName%\" -WingetID \"%WingetID%\""
+    "%~dp0PsExec64.exe" \\localhost /accepteula /s "%~dp0Toolkit\Deploy-Application.exe" -DeploymentType Install -DeployMode Silent -AllowRebootPassThru -WingetFriendlyName %WingetFriendlyName% -WingetID %WingetID%
 ) ELSE (
-    "%~dp0PsExec64.exe" /accepteula /s "\"%~dp0Toolkit\Deploy-Application.exe\" -DeploymentType Install -DeployMode Silent -AllowRebootPassThru -WingetFriendlyName \"%WingetFriendlyName%\" -WingetID \"%WingetID%\""
+    "%~dp0PsExec64.exe" /accepteula /s "%~dp0Toolkit\Deploy-Application.exe" -DeploymentType Install -DeployMode Silent -AllowRebootPassThru -WingetFriendlyName %WingetFriendlyName% -WingetID %WingetID%
 )
 TIMEOUT 10 >NUL
 GOTO :MENU
@@ -200,15 +200,15 @@ TIMEOUT 10 >NUL
 GOTO :MENU
 
 :Uninstall_silent_winget_psexec
-SET /P WingetFriendlyName=Enter Winget Friendly Name: 
+SET /P WingetFriendlyName=Enter Winget Friendly Name(Cannot contain spaces!): 
 SET /P WingetID=Enter Winget ID: 
 ECHO uninstall_silent_winget_psexec
 IF "%_SANDBOX%"=="Yes" (
     ECHO Machine is detected as Sandbox
     ECHO Friendly warning: Winget is not supported in Sandbox. So this will fail!
-    "%~dp0PsExec64.exe" \\localhost /accepteula /s "\"%~dp0Toolkit\Deploy-Application.exe\" -DeploymentType Uninstall -DeployMode Silent -AllowRebootPassThru -WingetFriendlyName \"%WingetFriendlyName%\" -WingetID \"%WingetID%\""
+    "%~dp0PsExec64.exe" \\localhost /accepteula /s "%~dp0Toolkit\Deploy-Application.exe" -DeploymentType Uninstall -DeployMode Silent -AllowRebootPassThru -WingetFriendlyName %WingetFriendlyName% -WingetID %WingetID%
 ) ELSE (
-    "%~dp0PsExec64.exe" /accepteula /s "\"%~dp0Toolkit\Deploy-Application.exe\" -DeploymentType Uninstall -DeployMode Silent -AllowRebootPassThru -WingetFriendlyName \"%WingetFriendlyName%\" -WingetID \"%WingetID%\""
+    "%~dp0PsExec64.exe" /accepteula /s "%~dp0Toolkit\Deploy-Application.exe" -DeploymentType Uninstall -DeployMode Silent -AllowRebootPassThru -WingetFriendlyName %WingetFriendlyName% -WingetID %WingetID%
 )
 TIMEOUT 10 >NUL
 GOTO :MENU
