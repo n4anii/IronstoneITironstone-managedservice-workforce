46977818c0ebde21a26ef3446a96302a50e71228
Herman Bergsløkken <herman.bergslokken@ironstoneit.com>
2024-10-07 13:50:54 +0200

    Make sure batch script checks for admin-rights

--- CWF2/Applications/Get-AutoPilot Info/Get-AutoPilot.bat
+++ CWF2/Applications/Get-AutoPilot Info/Get-AutoPilot.bat
@@ -1,8 +1,26 @@
-@echo off
-setlocal
+@ECHO OFF
+SETLOCAL
+
+TITLE Get Autopilot information
+
+::---------------------------------------------------------------------------------------------------------------------
+"%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system" >nul 2>&1 
+IF %ERRORLEVEL% NEQ 0 (
+CLS
+ECHO.
+ECHO.
+ECHO ***************************************************
+ECHO * This script needs to be run as administrator.   *
+ECHO * Please close the script, rightclick and choose  *
+ECHO *         "Run as administrator"                  *
+ECHO ***************************************************
+PAUSE
+EXIT
+)
+::---------------------------------------------------------------------------------------------------------------------
 
 :: Get the computer name
-set "computername=%COMPUTERNAME%"
+SET "computername=%COMPUTERNAME%"
 
 :: Export system information to a text file named after the computer name
 systeminfo > "%~dp0%computername%.txt"
@@ -12,4 +30,3 @@ tpmtool getdeviceinformation >> "%~dp0%computername%.txt"
 
 :: Run the PowerShell script
 powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0GetAutopilotInfo.ps1"
-
