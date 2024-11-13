0c78e15cec0168934a773f278716de78e5a51d0e
Herman Bergsløkken <herman.bergslokken@ironstoneit.com>
2024-11-13 16:23:35 +0100

    Reworke of ServiceUI to support custom parameters

--- CWF2/Applications/PSAppDeployToolkit - Template/Toolkit/Invoke-ServiceUI.ps1
+++ CWF2/Applications/PSAppDeployToolkit - Template/Toolkit/Invoke-ServiceUI.ps1
@@ -1,11 +1,11 @@
-<#
+﻿<#
 .SYNOPSIS
 This is a helper script to launch Deploy-Application.exe via ServiceUI to force the process to become visible when deployed by Intune, or other deployment systems that run in session 0.
 .DESCRIPTION
-This will launch the toolkit silently if the chosent process (explorer.exe by default) is not running. If it is running, then it will launch the toolkit interactively, and use ServiceUI to do so if the current process is non-interactive.
+This will launch the toolkit silently if the chosen process (explorer.exe by default) is not running. If it is running, then it will launch the toolkit interactively, and use ServiceUI to do so if the current process is non-interactive.
 An alternate ProcessName can be specified if you only want the toolkit to be visible when a specific application is running.
 Download MDT here: https://www.microsoft.com/en-us/download/details.aspx?id=54259
-There are x86 and x64 builds of ServiceUI avaiable in MDT under 'Microsoft Deployment Toolkit\Templates\Distribution\Tools'. Rename these to ServiceUI_x86.exe and ServiceUI_x64.exe and place them with this script in the root of the toolkit next to Deploy-Application.exe.
+There are x86 and x64 builds of ServiceUI available in MDT under 'Microsoft Deployment Toolkit\Templates\Distribution\Tools'. Rename these to ServiceUI_x86.exe and ServiceUI_x64.exe and place them with this script in the root of the toolkit next to Deploy-Application.exe.
 .PARAMETER ProcessName
 Specifies the name of the process check for to trigger the interactive installation. Default value is 'explorer'. Multiple values can be supplied such as 'app1','app2'. The .exe extension must be omitted.
 .PARAMETER DeploymentType
@@ -17,11 +17,11 @@ Passthru of switch to Deploy-Application.exe to enable terminal server mode.
 .PARAMETER DisableLogging
 Passthru of switch to Deploy-Application.exe to disable logging.
 .EXAMPLE
-.\Invoke-ServiceUI.ps1 -ProcessName 'WinSCP' -DeploymentType 'Install' -AllowRebootPassThru
-Invoking the script from the command line.
+.\Invoke-ServiceUI.ps1 -ProcessName 'WinSCP' -DeploymentType 'Install' -AllowRebootPassThru -AppWizName 'Value1'
+Invoking the script from the command line with custom parameters.
 .EXAMPLE
-%SystemRoot%\System32\WindowsPowerShell\v1.0\PowerShell.exe -ExecutionPolicy Bypass -NoProfile -File Invoke-ServiceUI.ps1 -DeploymentType Install -AllowRebootPassThru
-An example command line to use in Intune.
+%SystemRoot%\System32\WindowsPowerShell\v1.0\PowerShell.exe -ExecutionPolicy Bypass -NoProfile -File Invoke-ServiceUI.ps1 -DeploymentType Install -AllowRebootPassThru -CustomParameter 'Value2'
+An example command line to use in Intune with custom parameters.
 #>
 param (
     [string[]]$ProcessName = @('explorer'),
@@ -29,7 +29,10 @@ param (
     [string]$DeploymentType = 'Install',
     [switch]$AllowRebootPassThru,
     [switch]$TerminalServerMode,
-    [switch]$DisableLogging
+    [switch]$DisableLogging,
+    [string]$AppWizName,
+    [string]$WingetID,
+    [string]$CustomParameter
 )
 
 Push-Location $PSScriptRoot
@@ -43,15 +46,27 @@ if ($env:PROCESSOR_ARCHITECTURE -eq 'AMD64' -or $env:PROCESSOR_ARCHITEW6432 -eq 
 if (Get-Process -Name $ProcessName -ErrorAction SilentlyContinue) {
     if ([Environment]::UserInteractive) {
         # Start-Process is used here otherwise script does not wait for completion
-        $Process = Start-Process -FilePath '.\Deploy-Application.exe' -ArgumentList "-DeploymentType $DeploymentType -DeployMode Interactive -AllowRebootPassThru:`$$AllowRebootPassThru -TerminalServerMode:`$$TerminalServerMode -DisableLogging:`$$DisableLogging" -NoNewWindow -Wait -PassThru
+        $Arguments = "-DeploymentType $DeploymentType -DeployMode Interactive -AllowRebootPassThru:`$$AllowRebootPassThru -TerminalServerMode:`$$TerminalServerMode -DisableLogging:`$$DisableLogging"
+        if ($AppWizName) { $Arguments += " -AppWizName $AppWizName" }
+        if ($WingetID) { $Arguments += " -WingetID $WingetID" }
+        if ($CustomParameter) { $Arguments += " -CustomParameter $CustomParameter" }
+        $Process = Start-Process -FilePath '.\Deploy-Application.exe' -ArgumentList $Arguments -NoNewWindow -Wait -PassThru
         $ExitCode = $Process.ExitCode
     } else {
         # Using Start-Process with ServiceUI results in Error Code 5 (Access Denied)
-        &".\ServiceUI_$Architecture.exe" -process:explorer.exe Deploy-Application.exe -DeploymentType $DeploymentType -DeployMode Interactive -AllowRebootPassThru:"`$$AllowRebootPassThru" -TerminalServerMode:"`$$TerminalServerMode" -DisableLogging:"`$$DisableLogging"
+        $Arguments = "-DeploymentType $DeploymentType -DeployMode Interactive -AllowRebootPassThru:`$$AllowRebootPassThru -TerminalServerMode:`$$TerminalServerMode -DisableLogging:`$$DisableLogging"
+        if ($AppWizName) { $Arguments += " -AppWizName $AppWizName" }
+        if ($WingetID) { $Arguments += " -WingetID $WingetID" }
+        if ($CustomParameter) { $Arguments += " -CustomParameter $CustomParameter" }
+        &".\ServiceUI_$Architecture.exe" -process:explorer.exe Deploy-Application.exe $Arguments
         $ExitCode = $LastExitCode
     }
 } else {
-    $Process = Start-Process -FilePath '.\Deploy-Application.exe' -ArgumentList "-DeploymentType $DeploymentType -DeployMode Silent -AllowRebootPassThru:`$$AllowRebootPassThru -TerminalServerMode:`$$TerminalServerMode -DisableLogging:`$$DisableLogging" -NoNewWindow -Wait -PassThru
+    $Arguments = "-DeploymentType $DeploymentType -DeployMode Silent -AllowRebootPassThru:`$$AllowRebootPassThru -TerminalServerMode:`$$TerminalServerMode -DisableLogging:`$$DisableLogging"
+    if ($AppWizName) { $Arguments += " -AppWizName $AppWizName" }
+    if ($WingetID) { $Arguments += " -WingetID $WingetID" }
+    if ($CustomParameter) { $Arguments += " -CustomParameter $CustomParameter" }
+    $Process = Start-Process -FilePath '.\Deploy-Application.exe' -ArgumentList $Arguments -NoNewWindow -Wait -PassThru
     $ExitCode = $Process.ExitCode
 }
 
